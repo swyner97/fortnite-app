ecfddf3387d39dc8909650ab0aac0d93870b8433
andresilva8624@gmail.com <andresilva8624@gmail.com>
2022-08-11 21:07:04 -0700

    updated javascript with and array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 // Submit button variable and function
 let submitBtnEl = $("#submit-btn");
 
+
 submitBtnEl.on("click", function (event) {
   let userSearch = $(this).siblings("#searched-item").val();
   localStorage.setItem("item-request", userSearch);
@@ -16,6 +17,20 @@ submitBtnEl.on("click", function (event) {
     .then((response) => console.log(response))
     .catch((err) => console.error(err));
 });
+
+
+function insert(arr, index, ...items)
+{
+    return [
+            ...arr.slice(0, index),
+            ...items,
+            ...arr.slice(index)
+        ];
+}
+ 
+var arr = [3, 4, 5];
+var result = insert(arr, 0, 1, 2);
+console.log(result);
 
 // fetch('https://fortnite-api.com/v2/shop/br').then(res =>{
 //     console.log(res)
