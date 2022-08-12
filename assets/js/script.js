08105d15f116d91ebe43f7e65f98ed8cdcc25ffb
Clay McKee <cmckee120993@gmail.com>
2022-08-11 22:00:54 -0700

    (Clay) We worked together with Meg and Matty to find a solution as to why our API was returning an error. We now have to get access from a cors site every time we work on the project for it to work. This must also be documented on our README for users.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,24 @@ submitBtnEl.on("click", function (event) {
   $(this).siblings("#searched-item").val("");
 
   const options = {
-    method: "GET",
-    headers: { "TRN-API-Key": " e486b319-39d4-49b9-a3f1-ee5151dd4bb6" },
+    method: 'GET',
+    headers: {
+      'TRN-Api-Key': 'e486b319-39d4-49b9-a3f1-ee5151dd4bb6',
+    //   'Access-Control-Allow-Origin': '*',
+    },
+    // mode: 'no-cors'
   };
-
-  fetch("https://api.fortnitetracker.com/v1/store", options)
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
+  
+  fetch('https://cors-anywhere.herokuapp.com/https://api.fortnitetracker.com/v1/store', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
 });
+
+// In order to get access to this api, we have to go to this link (cors-anywhere.herokuapp.com) and get access every day/every time we work on the project. 
+// We also have to put this in our README for the grading team so they can refresh before grading. 
+
+
 
 
 function insert(arr, index, ...items)
@@ -31,6 +40,10 @@ function insert(arr, index, ...items)
 var arr = [3, 4, 5];
 var result = insert(arr, 0, 1, 2);
 console.log(result);
+
+
+// I believe this is the api for the current items rather than the full store.
+// I will keep it here just in case
 
 // fetch('https://fortnite-api.com/v2/shop/br').then(res =>{
 //     console.log(res)
