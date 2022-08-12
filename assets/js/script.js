6eeb2506a8d09685dd32e9e693bad65c5281837e
andresilva8624@gmail.com <andresilva8624@gmail.com>
2022-08-11 20:27:15 -0700

    updated fetch request again

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,24 @@
 // Submit button variable and function
-let submitBtnEl = $('#submit-btn');
+let submitBtnEl = $("#submit-btn");
 
-submitBtnEl.on('click', function(event) {
-    let userSearch = $(this).siblings('#searched-item').val();
-    localStorage.setItem('item-request', userSearch);
-    $(this).siblings('#searched-item').val('');
+submitBtnEl.on("click", function (event) {
+  let userSearch = $(this).siblings("#searched-item").val();
+  localStorage.setItem("item-request", userSearch);
+  $(this).siblings("#searched-item").val("");
+
+  const options = {
+    method: "GET",
+    headers: { "TRN-API-Key": " e486b319-39d4-49b9-a3f1-ee5151dd4bb6" },
+  };
+
+  fetch("https://api.fortnitetracker.com/v1/store", options)
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
 });
-
-
-
 
 // fetch('https://fortnite-api.com/v2/shop/br').then(res =>{
 //     console.log(res)
-    
+
 // console.log(data)
 // })
