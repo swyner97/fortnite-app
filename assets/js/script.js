ebcd9b6856296e6f7db94fb6087c36f85256da9f
andresilva8624@gmail.com <andresilva8624@gmail.com>
2022-08-15 21:14:20 -0700

    updated div

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,25 @@
+
+let key = config.SECRET_API_KEY;
+let url = 'https://cors-anywhere.herokuapp.com/https://fortnite-api.com/v2/shop/br/combined?entries=items&limit=10';
+
+
+let getAPI = (url) => {
+    const options = {method: 'GET'};
+
+    fetch('https://fortnite-api.com/v2/shop/br', options)
+      .then(response => response.json())
+      .then(response => console.log(response.data))
+      .catch(err => console.error(err));
+}
+
+getAPI();
+
+
 // Submit button variable and function
 let submitBtnEl = $("#submit-btn");
 
 
-submitBtnEl.on("click", function (event) {
+function currentItemStore (event) {
   let userSearch = $(this).siblings("#searched-item").val();
   localStorage.setItem("item-request", userSearch);
 
@@ -15,10 +32,33 @@ submitBtnEl.on("click", function (event) {
     .then((response) => response.json())
     .then((response) => console.log(response))
     .catch((err) => console.error(err));
-});
+};
+
+  fetch("https://cors-anywhere.herokuapp.com/https://api.fortnitetracker.com/v1/store", options)
+    .then((response) => response.json())
+    .then(function(data) {
+      console.log(data);
+      for (var i=0; i<data.length; i++) {
+        let displayEl = $('.HTMLPLACEHOLDER')
+        let currentItemCardsEl = $(`<div></div>`);
+        let itemImageEl  = $(`<img src='${data[i].imageUrl}'/>`);
+        let itemNameEl = $(`<h3>${data[i].name}</h3>`);
+        let itemRarityEl = $(`<h4>${data[i].rarity}</h4>`);
+        let itemCostEl = $(`<h4>${data[i].vBucks}</h4>`);
+        let wishlistBtnEl = $(`<button class=wishlist-btn>Add to Wishlist</button>`);
+
+        currentItemCardsEl.append(itemImageEl, itemNameEl, itemRarityEl, itemCostEl, wishlistBtnEl);
+        displayEl.append(currentItemCardsEl);
+      }
+    })
+    .catch((err) => console.error(err)); 
+
+  currentItemStore();
+
 
 // In order to get access to this api, we have to go to this link (cors-anywhere.herokuapp.com) and get access every day/every time we work on the project. 
 // We also have to put this in our README for the grading team so they can refresh before grading. 
+
 
 
 
