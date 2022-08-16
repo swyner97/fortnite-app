485c2238c655fd3839ac553ef2ad8b187721cad9
Clay McKee <cmckee120993@gmail.com>
2022-08-15 20:49:06 -0700

    removed submit button code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,13 @@
 
-let key = config.SECRET_API_KEY;
-let url = 'https://cors-anywhere.herokuapp.com/https://fortnite-api.com/v2/shop/br/combined?entries=items&limit=10';
-
-
-let getAPI = (url) => {
-    const options = {method: 'GET'};
-
-    fetch('https://fortnite-api.com/v2/shop/br', options)
-      .then(response => response.json())
-      .then(response => console.log(response.data))
-      .catch(err => console.error(err));
-}
-
-getAPI();
-
 // Submit button variable and function
-let submitBtnEl = $("#submit-btn");
 
 
 function currentItemStore (event) {
-  let userSearch = $(this).siblings("#searched-item").val();
-  localStorage.setItem("item-request", userSearch);
-
+  
   const options = {
     method: "GET",
     headers: { "TRN-API-Key": "e486b319-39d4-49b9-a3f1-ee5151dd4bb6"},
   };
-
-
-  fetch("https://crossorigin.me/https://api.fortnitetracker.com/v1/store", options)
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
-});
 
   fetch("https://cors-anywhere.herokuapp.com/https://api.fortnitetracker.com/v1/store", options)
     .then((response) => response.json())
