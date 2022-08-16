ccf71cfa0d38fdb9c0174172de65b41b23f8e8f4
andresilva8624@gmail.com <andresilva8624@gmail.com>
2022-08-15 21:49:05 -0700

    updated html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,13 @@
-// let key = config.SECRET_API_KEY;
+let key = config.SECRET_API_KEY;
 let url = 'https://cors-anywhere.herokuapp.com/https://fortnite-api.com/v2/shop/br/combined?entries=items&limit=10';
 
-
 let getAPI = (url) => {
-    const options = {method: 'GET'};
+  const options = { method: 'GET' };
 
-    fetch('https://fortnite-api.com/v2/shop/br', options)
-      .then(response => response.json())
-      .then(response => console.log(response.data))
-      .catch(err => console.error(err));
+  fetch('https://fortnite-api.com/v2/shop/br', options)
+    .then(response => response.json())
+    .then(response => console.log(response.data))
+    .catch(err => console.error(err));
 }
 
 getAPI();
@@ -16,21 +15,21 @@ getAPI();
 // Submit button variable and function
 
 
-function currentItemStore (event) {
-  
+function currentItemStore(event) {
   const options = {
     method: "GET",
-    headers: { "TRN-API-Key": "e486b319-39d4-49b9-a3f1-ee5151dd4bb6"},
+    headers: { "TRN-API-Key": "e486b319-39d4-49b9-a3f1-ee5151dd4bb6" },
   };
 
-fetch("https://cors-anywhere.herokuapp.com/https://api.fortnitetracker.com/v1/store", options)
+
+  fetch("https://cors-anywhere.herokuapp.com/https://api.fortnitetracker.com/v1/store", options)
     .then((response) => response.json())
-    .then(function(data) {
+    .then(function (data) {
       console.log(data);
-      for (var i=0; i<data.length; i++) {
+      for (var i = 0; i < data.length; i++) {
         let displayEl = $('.HTMLPLACEHOLDER')
         let currentItemCardsEl = $(`<div></div>`);
-        let itemImageEl  = $(`<img src='${data[i].imageUrl}'/>`);
+        let itemImageEl = $(`<img src='${data[i].imageUrl}'/>`);
         let itemNameEl = $(`<h3>${data[i].name}</h3>`);
         let itemRarityEl = $(`<h4>${data[i].rarity}</h4>`);
         let itemCostEl = $(`<h4>${data[i].vBucks}</h4>`);
@@ -41,11 +40,11 @@ fetch("https://cors-anywhere.herokuapp.com/https://api.fortnitetracker.com/v1/st
       }
     })
     .catch((err) => console.error(err));
-  }; 
+};
 
-  currentItemStore();
+currentItemStore();
+
 
 
 // In order to get access to this api, we have to go to this link (cors-anywhere.herokuapp.com) and get access every day/every time we work on the project. 
 // We also have to put this in our README for the grading team so they can refresh before grading. 
-
