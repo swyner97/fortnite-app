e14be8652930c26c236d6b6acce7d0af34455d26
swyner97 <swyner97@gmail.com>
2022-08-15 19:54:32 -0700

    figured out issue with retrieving data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,26 @@
 let key = config.SECRET_API_KEY;
-let url = 'https://fortnite-api.com/v2/shop/br?daily=entires&limit=6';
+let url = 'https://cors-anywhere.herokuapp.com/https://fortnite-api.com/v2/shop/br/combined?entries=items&limit=10';
 
-fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data)
-        });
+// function getAPI(url) {
+//     fetch(url)
+//         .then(function (response) {
+//             response.json()
+//         })
+//         .then(function (data) {
+//             console.log(data)
+//         });
+// }
+
+
+// getAPI();
+
+function getAPI(url) {
+    const options = {method: 'GET'};
+
+    fetch('https://fortnite-api.com/v2/shop/br', options)
+      .then(response => response.json())
+      .then(response => console.log(response.data.featured.entries))
+      .catch(err => console.error(err));
+}
+
+getAPI();
