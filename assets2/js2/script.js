f79ac64feace228ccdc9feb6dd5635d0d1f00fe7
andresilva8624@gmail.com <andresilva8624@gmail.com>
2022-08-18 20:52:55 -0700

    finalizing layout

--- assets2/js2/script.js
+++ assets2/js2/script.js
@@ -1,70 +1,107 @@
 var myHeaders = new Headers();
 myHeaders.append("Authorization", "{{authorization}}");
 
-
 let getAllItems = () => {
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
 
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
+  fetch("https://fortnite-api.theapinetwork.com/store/get", requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < data.data.length; i++) {
+        let itemName = data.data[i].item.name.toString();
+        let itemInfo = data.data[i].item.description;
+        let itemPrice = data.data[i].store.cost;
+        let imgEl = document.createElement("img");
+        imgEl.setAttribute("src", data.data[i].item.images.icon);
+        let itemList = document.createElement("li");
+        let itemUl = document.querySelector(".all-items");
+        console.log(data.data[i].item.images.featured);
 
-    fetch("https://fortnite-api.theapinetwork.com/store/get", requestOptions)
-        .then(response => response.json())
-        .then(data => {
+        itemList.append(itemName, itemInfo, itemInfo, itemPrice, imgEl);
+        itemUl.append(itemList);
+      }
+    })
+    .catch((error) => console.log("error", error));
+};
 
-            for (let i = 0; i < data.data.length; i++) {
-                let itemName = data.data[i].item.name.toString();
-                let itemInfo = data.data[i].item.description;
-                let itemPrice = data.data[i].store.cost;
-                let imgEl = document.createElement('img');
-                imgEl.setAttribute('src', data.data[i].item.images.icon);
-                let itemList = document.createElement('li');
-                let itemUl = document.querySelector('.all-items')
-                console.log(data.data[i].item.images.featured);
-
-                itemList.append(itemName, itemInfo, itemInfo, itemPrice, imgEl);
-                itemUl.append(itemList)
-            }
-        }
-        )
-        .catch(error => console.log('error', error));
-}
-
-getAllItems()
+getAllItems();
 
 // Populate current items from API
 
-
 function currentItemStore(event) {
-    const options = {
-        method: "GET",
-        headers: { "TRN-API-Key": "e486b319-39d4-49b9-a3f1-ee5151dd4bb6" },
-    };
+  const options = {
+    method: "GET",
+    headers: { "TRN-API-Key": "e486b319-39d4-49b9-a3f1-ee5151dd4bb6" },
+  };
 
+  fetch(
+    "https://cors-anywhere.herokuapp.com/https://api.fortnitetracker.com/v1/store",
+    options
+  )
+    .then((response) => response.json())
+    .then(function (data) {
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        // gallery from HTML
+        let displayEl = $("#current-gallery");
+        // card div
+        let currentItemCardsEl = $(`<div class="card" id="item-card"></div>`);
+        // img
+        let itemImgDiv = $(`<div class='card-image'></div>`);
+        let itemImageEl = $(
+          `<figure class='image is-3by2'><img src='${data[i].imageUrl}'/></figure>`
+        );
+        itemImgDiv.append(itemImageEl);
+        // card content
+        let itemContentDiv = $(`<div class='card-content'></div>`);
+        let itemNameEl = $(`<h3>${data[i].name}</h3>`);
+        let itemRarityEl = $(`<h4>${data[i].rarity}</h4>`);
+        let itemCostEl = $(`<h4>${data[i].vBucks}</h4>`);
+        itemContentDiv.append(itemNameEl, itemRarityEl, itemCostEl);
+        // button
+        let wishlistBtnEl = $(
+          `<button class=wishlist-btn>Add to Wishlist</button>`
+        );
 
-    fetch("https://cors-anywhere.herokuapp.com/https://api.fortnitetracker.com/v1/store", options)
-        .then((response) => response.json())
-        .then(function (data) {
-            console.log(data);
-            for (var i = 0; i < data.length; i++) {
-                let displayEl = $('.current-store-gallery')
-                let currentItemCardsEl = $(`<div class="w3-padding w3-margin item-card"></div>`);
-                let itemImageEl = $(`<img src='${data[i].imageUrl}'/>`);
-                let itemNameEl = $(`<h3>${data[i].name}</h3>`);
-                let itemRarityEl = $(`<h4>${data[i].rarity}</h4>`);
-                let itemCostEl = $(`<h4>${data[i].vBucks}</h4>`);
-                let wishlistBtnEl = $(`<button class=wishlist-btn>Add to Wishlist</button>`);
-
-                currentItemCardsEl.append(itemImageEl, itemNameEl, itemRarityEl, itemCostEl, wishlistBtnEl);
-                displayEl.append(currentItemCardsEl);
-            }
-        })
-        .catch((err) => console.error(err));
-};
+        currentItemCardsEl.append(itemImgDiv, itemContentDiv, wishlistBtnEl);
+        displayEl.append(currentItemCardsEl);
+      }
+    })
+    .catch((err) => console.error(err));
+}
 
 currentItemStore();
+
+// Slideshow functionality
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("item-slide");
+  //   var captionText = document.getElementById("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+}
 
 // Local storage for wishlist/searchbar
 var wishlistAdder = document.querySelector(".wishlistAdder");
@@ -75,53 +112,49 @@ var searchButton = document.querySelector("#searchBtn");
 var wishlist = [];
 var lastSearched = [];
 
-function renderWishlist() {
-
-}
+function renderWishlist() {}
 
 function init() {
-    var storedWishlist = JSON.parse(localstorage.getItem("wishlist"));
-    var storedSearches = JSON.parse(localStorage.getItem("lastSearched"));
+  var storedWishlist = JSON.parse(localstorage.getItem("wishlist"));
+  var storedSearches = JSON.parse(localStorage.getItem("lastSearched"));
 
-    if (storedWishlist !== null || storedSearches !== null) {
-        wishlist = storedWishlist;
-        lastSearched = storedSearches;
-    };
-};
+  if (storedWishlist !== null || storedSearches !== null) {
+    wishlist = storedWishlist;
+    lastSearched = storedSearches;
+  }
+}
 
 function addToWishlist() {
-    localStorage.setItem("wishlist", JSON.stringify(wishlist));
-};
+  localStorage.setItem("wishlist", JSON.stringify(wishlist));
+}
 
 wishlistAdder.addEventListener("click", function () {
-    wishlist.push(itemName);
+  wishlist.push(itemName);
 
-    addToWishlist();
-    renderWishlist();
+  addToWishlist();
+  renderWishlist();
 });
 
 function storeLastSearched() {
-    localStorage.setItem("lastSearched", JSON.stringify(lastSearched))
-};
+  localStorage.setItem("lastSearched", JSON.stringify(lastSearched));
+}
 
 searchButton.addEventListener("click", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var searchText = searchBarInput.value.trim();
+  var searchText = searchBarInput.value.trim();
 
-    if (searchText === "") {
-        return;
-    };
+  if (searchText === "") {
+    return;
+  }
 
-    lastSearched.push(searchText);
-    searchBarInput.value = "";
+  lastSearched.push(searchText);
+  searchBarInput.value = "";
 
-    storeLastSearched();
+  storeLastSearched();
 });
 
 init();
 
-
-
-  // In order to get access to this api, we have to go to this link (cors-anywhere.herokuapp.com) and get access every day/every time we work on the project.
-  // We also have to put this in our README for the grading team so they can refresh before grading.
+// In order to get access to this api, we have to go to this link (cors-anywhere.herokuapp.com) and get access every day/every time we work on the project.
+// We also have to put this in our README for the grading team so they can refresh before grading.
